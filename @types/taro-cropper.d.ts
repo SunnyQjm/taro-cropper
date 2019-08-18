73d47d13baa67b2d3b9f23bb867b52434232f628
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-08-18 09:54:21 +0800

    add: 添加d.ts，发布到npm

---
+++ @types/taro-cropper.d.ts
@@ -0,0 +1,17 @@
+import {ComponentClass} from "react";
+
+export interface TaroCropperComponentProps {
+  cropperCanvasId?: string,  // 裁剪框画布id
+  width?: number,            // 组件宽度
+  height?: number,           // 组件高度   (要求背景高度大于宽度)
+  cropperWidth?: number,     // 裁剪框宽度
+  cropperHeight?: number,    // 裁剪框高度
+  themeColor?: string,       // 主题色（裁剪框的四个角的绘制颜色）
+  maxScale?: number,         // 最大放大倍数，maxScale >= 1
+  fullScreen?: boolean,      // 组件充满全屏，此时width和height设置无效
+  src: string,              // 要裁剪的图片路径
+}
+
+declare const TaroCropper: ComponentClass<TaroCropperComponentProps>;
+
+export default TaroCropper;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export {default as TaroCropper} from './components/taro-cropper';
