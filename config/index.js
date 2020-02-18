71ca523a8d74214d9bd3239ca64cec04e509822e
SunnyQjm <qjm253@pku.edu.cn>
2020-02-18 19:52:38 +0800

    update: 升级taro版本至2.0.4

--- config/index.js
+++ config/index.js
@@ -9,60 +9,31 @@ const config = {
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: {
-    babel: {
-      sourceMap: true,
-      presets: [
-        ['env', {
-          modules: false
-        }]
-      ],
-      plugins: [
-        'transform-decorators-legacy',
-        'transform-class-properties',
-        'transform-object-rest-spread'
-      ]
-    }
-  },
-  defineConstants: {
-  },
-  copy: {
-    patterns: [
+  babel: {
+    sourceMap: true,
+    presets: [
+      ['env', {
+        modules: false
+      }]
     ],
-    options: {
-    }
+    plugins: [
+      'transform-decorators-legacy',
+      'transform-class-properties',
+      'transform-object-rest-spread'
+    ]
   },
-  weapp: {
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: [
-              'last 3 versions',
-              'Android >= 4.1',
-              'ios >= 8'
-            ]
-          }
-        },
-        pxtransform: {
-          enable: true,
-          config: {
-
-          }
-        },
-        url: {
-          enable: true,
-          config: {
-            limit: 10240 // 设定转换尺寸上限
-          }
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
+  mini: {
+    webpackChain (chain, webpack) {},
+    cssLoaderOption: {},
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {}
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 10240 // 设定转换尺寸上限
         }
       }
     }
@@ -70,24 +41,16 @@ const config = {
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: [
-              'last 3 versions',
-              'Android >= 4.1',
-              'ios >= 8'
-            ]
-          }
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
+    webpackChain (chain, webpack) {},
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+          browsers: [
+            'last 3 versions',
+            'Android >= 4.1',
+            'ios >= 8'
+          ]
         }
       }
     }
@@ -99,10 +62,10 @@ if (process.env.TARO_BUILD_TYPE === 'ui') {
     enableSourceMap: false,
     enableExtract: false,
     enableDll: false
-  })
+  });
   config.h5.webpackChain = chain => {
-    chain.plugins.delete('htmlWebpackPlugin')
-    chain.plugins.delete('addAssetHtmlWebpackPlugin')
+    chain.plugins.delete('htmlWebpackPlugin');
+    chain.plugins.delete('addAssetHtmlWebpackPlugin');
     chain.merge({
       output: {
         path: path.join(process.cwd(), 'dist', 'h5'),
@@ -126,4 +89,4 @@ module.exports = function (merge) {
     return merge({}, config, require('./dev'))
   }
   return merge({}, config, require('./prod'))
-}
+};
